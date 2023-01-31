d6c4389f9bd570bc59ab17a0938db78eb1c371fb
Michael Butler <butler@cockroachlabs.com>
2023-01-31 12:09:38 -0500

    bank fixture

--- backupFixtures/morevars.sh
+++ backupFixtures/morevars.sh
@@ -5,13 +5,16 @@
 
 let total_nodes=$crdb_nodes+1
 
-# workload variables
+# workload variables: should be set after config variables
 if [[ "$workload" == "tpc-e" ]]; then
   init_cmd="sudo docker run cockroachdb/tpc-e:latest --init --customers=$workload_val --racks=$crdb_nodes \$(cat hosts.txt)"
   run_cmd="sudo docker run cockroachdb/tpc-e:latest --customers=$workload_val --racks=$crdb_nodes --duration=$duration \$(cat hosts.txt)"
 elif [[ "$workload" == "tpc-c" ]]; then
   init_cmd="./workload init tpcc --data-loader import --warehouses $workload_val {pgurl:1-$crdb_nodes}" 
-  run_cmd="./workload run tpcc --warehouses=$workload_val --duration=$duration --tolerate-errors {pgurl:1-$crdb_nodes}"
+  run_cmd="./workload run tpcc --warehouses=$tpcc_run_val --duration=$duration --tolerate-errors {pgurl:1-$crdb_nodes}"
+elif [[ "$workload" == "bank" ]]; then
+  init_cmd="./workload init bank --data-loader import --rows=$workload_val --payload-bytes=$bank_payload_bytes {pgurl:1-$crdb_nodes}" 
+  run_cmd="./workload run bank --payload-bytes=$bank_payload_bytes --duration=$duration --tolerate-errors {pgurl:1-$crdb_nodes}"
 fi
 
 # backup variables
