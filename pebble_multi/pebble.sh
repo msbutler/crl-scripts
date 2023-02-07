da2562fed8a75105ee2dbe89a9587d6408473041
Michael Butler <butler@cockroachlabs.com>
2023-02-07 10:37:04 -0500

    pebble script update

--- pebble_multi/pebble.sh
+++ pebble_multi/pebble.sh
@@ -8,8 +8,14 @@ set -e
 
 binary=$1
 workers=$2
+verboseArg=$3
 name=$CLUSTER-$binary-$workers
 maxSize=300000
+
+verbose=""
+if [[ "$verboseArg" == "verbose" ]]; then
+  verbose="-v"
+fi
 
 # Create a good roachprod node
 roachprod create $name --nodes=1 --gce-machine-type=n2-standard-16 --local-ssd=false --gce-pd-volume-size=2000
@@ -18,7 +24,7 @@ roachprod create $name --nodes=1 --gce-machine-type=n2-standard-16 --local-ssd=f
 roachprod put $name $binary.o
 
 # Run the workload until lsm is 300 GB
-roachprod run $name -- "tmux new -d -s ycsb \"./$binary.o bench ycsb /mnt/data1 --workload F --initial-keys 100000 -c $workers --max-size=$maxSize -d 0m --wait-compactions > output.txt\""
+roachprod run $name -- "tmux new -d -s ycsb \"./$binary.o bench ycsb /mnt/data1 --workload F --initial-keys 100000 -c $workers --max-size=$maxSize -d 0m --wait-compactions $verbose 1> output.txt 2>verbose.txt\""
 
 echo "Began ycsb workload:
 roachprod cluster: $name
