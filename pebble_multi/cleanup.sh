aa98782dd2a6570d50c2381c3408d5c1f51d90f6
Michael Butler <butler@cockroachlabs.com>
2023-02-22 14:44:46 -0500

    update scripts

--- pebble_multi/cleanup.sh
+++ pebble_multi/cleanup.sh
@@ -6,8 +6,10 @@ workers=$2
 name=$CLUSTER-$binary-$workers
 dir=$binary-$workers
 echo "$dir"
-mkdir -p $dir  
-roachprod get $name output.txt $dir/output.txt 
-roachprod get $name verbose.txt $dir/verbose.txt
+mkdir -p res/$dir  
+roachprod get $name output.txt res/$dir/output.txt 
+roachprod get $name verbose.txt res/$dir/verbose.txt
+roachprod destroy $name
+
 
 
--- pebble_multi/orch.sh
+++ pebble_multi/orch.sh
@@ -3,10 +3,15 @@ set -e
 
 roachprod list | grep "butler"
 ./$1.sh control 96 verbose
-./$1.sh control 124 verbose
+./$1.sh control 128 verbose
 ./$1.sh multi 96 verbose
-./$1.sh multi 124 verbose
+./$1.sh multi 128 verbose
 ./$1.sh multi-counter-l0 96 verbose
+./$1.sh multi-counter-l0 128 verbose
 ./$1.sh multi-l0 96 verbose
-./$1.sh multi-l0 124 verbose
+./$1.sh multi-l0 128 verbose
+
+if [[ "$1" == "cleanup" ]]; then
+  zip -r res.zip res
+fi
 
