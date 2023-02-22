441c66270a43cef4994bdf9ef090b6de39e08738
Michael Butler <butler@cockroachlabs.com>
2023-02-22 20:11:21 +0000

    add binaries

--- pebble_multi/pebble.sh
+++ pebble_multi/pebble.sh
@@ -21,7 +21,7 @@ fi
 roachprod create $name --nodes=1 --gce-machine-type=n2-standard-16 --local-ssd=false --gce-pd-volume-size=2000
 
 # Push the  _linux_  binaries to roachprod (build from gce worker)
-roachprod put $name $binary.o
+roachprod put $name binaries/$binary.o
 
 # Run the workload until lsm is 300 GB
 roachprod run $name -- "tmux new -d -s ycsb \"./$binary.o bench ycsb /mnt/data1 --workload F --initial-keys 100000 -c $workers --max-size=$maxSize -d 0m --wait-compactions $verbose 1> output.txt 2>output.txt\""
