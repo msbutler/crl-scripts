26dd8cb7a2d38dd46e255ce3afb2c22606d80754
Michael Butler <butler@cockroachlabs.com>
2023-04-25 15:31:25 +0000

    replay updates

--- pebble_replay/replay.sh
+++ pebble_replay/replay.sh
@@ -5,4 +5,4 @@ DISK_NAME="$USER-kv0"
 pebble_repo="/home/michaelbutler/go/src/github.com/cockroachdb/pebble"
 cp $pebble_repo/$1.o $1.o
 
-./$1.o bench replay -v "/mnt/disks/$DISK_NAME/store_1/20230117212340" --name kv0/20gb --stream-logs --count 6 --options "[Options] max_concurrent_compactions=5" --pacer=reference-ramp --max-writes 20480 1>replay_logs/$1.txt 2>replay_logs/$1_verbose.txt
+./$1.o bench replay -v "/mnt/disks/$DISK_NAME/store_1/20230117212340" --name kv0/20gb --stream-logs --count 6 --options "[Options] max_concurrent_compactions=5" --pacer=reference-ramp --max-writes 20480 1>replay_logs/$1.log 2>replay_logs/$1_verbose.log
