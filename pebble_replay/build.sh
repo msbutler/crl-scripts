b8991806ba83dd71a4c86a1739a4f11870803b84
Michael Butler <butler@cockroachlabs.com>
2023-04-26 18:49:50 +0000

    update replay script

---
+++ pebble_replay/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+#build test branches
+
+pwd=$(pwd)
+pebble_repo="/home/michaelbutler/go/src/github.com/cockroachdb/pebble"
+cd $pebble_repo
+grr
+g switch butler-metrics-explore
+
+# with all commits, build multi level with l0 input
+go build -o multi-l0.o ./cmd/pebble
+
+# remove top commit and buiild with l0 input
+git reset HEAD~1 --hard
+go build -o multi.o ./cmd/pebble
+
+# remove top commit and build control
+git reset HEAD~1 --hard
+go build -o control.o ./cmd/pebble
+
+cd $pwd
+
+
+
+
