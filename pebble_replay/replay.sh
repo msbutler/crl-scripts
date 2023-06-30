4290e3357cfe913725ffaf8303c462eb597844b4
Michael Butler <butler@cockroachlabs.com>
2023-06-30 14:27:50 +0000

    one more typo

--- pebble_replay/replay.sh
+++ pebble_replay/replay.sh
@@ -4,10 +4,11 @@ set -e
 # This script grabs the target binary from the pebble repo and runs the replay
 # bench.
 
-DISK_NAME="$USER-kv0short"
+. ./config.sh
+
 pebble_repo="/home/michaelbutler/go/src/github.com/cockroachdb/pebble"
 cp $pebble_repo/$1.o $1.o
 
-./$1.o bench replay -v "/mnt/disks/$DISK_NAME/store_1/20230117212340" --name kv0/20gb --stream-logs --count 6 --options "[Options] max_concurrent_compactions=5" --pacer=reference-ramp --max-writes 20480 1>replay_logs/$1.log 2>replay_logs/$1_verbose.log
+./$1.o bench replay -v "/mnt/disks/$DISK_NAME/store_1/20230117212340" --name kv0/20gb --stream-logs --count 5 --options "[Options] max_concurrent_compactions=5" --pacer=reference-ramp --max-writes 20480 1>replay_logs/$1.log 2>replay_logs/$1_verbose.log
 
 sudo /usr/sbin/fstrim --fstab --verbose --quiet
